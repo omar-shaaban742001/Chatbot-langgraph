9c734a299e65bbb367c37733a37ff89f01fadae7
omar-shaaban742001 <omarshaaban74001@gmail.com>
2026-09-14 11:17:09 +0300

    add a simple chatbot

---
+++ chatbot_backend.py
@@ -0,0 +1,46 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_ollama import ChatOllama
+from dotenv import load_dotenv
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph.message import add_messages
+
+
+load_dotenv()
+
+model = ChatOllama(
+    model="qwen2.5:0.5b",
+    temperature=0
+)
+
+
+class ChatState(TypedDict):
+
+    messages: Annotated[list[BaseMessage], add_messages]
+
+
+
+def chat_node(state: ChatState):
+    #take user query from state
+    messages = state['messages']
+    # send to llm
+    response = model.invoke(messages)
+    # response store state
+    return {'messages': [response]}
+
+
+
+checkpoint = MemorySaver()
+
+graph = StateGraph(ChatState)
+
+# add nodes
+graph.add_node('chat_node', chat_node)
+
+#add edges
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+
+chatbot = graph.compile(checkpointer=checkpoint)
